bf2fa7a3d0729686e63f601865f74967d1517a20
chrissi9290 <wachaufc@gail.com>
2025-03-05 07:09:11 +0100

    Initialer Upload des SolSniper Bot Projekts

---
+++ README.md
@@ -0,0 +1,2 @@
+# SolSniper Bot
+Ein automatisierter Meme-Coin Sniper Bot für Pump.fun und Raydium.
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SolSniper Bot Frontend läuft!</h1>
+    </div>
+  );
+}
+
+export default App;
